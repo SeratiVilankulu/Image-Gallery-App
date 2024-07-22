984143e005cfa329e2c994d58a3ba7d42e0a9518
Serati Vilankulu <seratimotla@gmail.com>
2024-07-22 08:26:04 +0200

    changes

--- Test_api/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ Test_api/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba78a90ba05127bee12018ff5a6de582febe16ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e60844548366ba5123cfa0c2050689095ad1c5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
